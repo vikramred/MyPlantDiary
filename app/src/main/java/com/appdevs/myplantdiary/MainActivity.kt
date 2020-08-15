2f27e1ca8ccd087bf530fd9cf1c3348114143000
vikramred <vickywick123@gmail.com>
2020-08-16 02:27:18 +0530

    sdfsdf

---
+++ app/src/main/java/com/appdevs/myplantdiary/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.appdevs.myplantdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.appdevs.myplantdiary.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
